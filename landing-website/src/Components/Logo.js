b202d564ff10e0054419560d7495a1bfaeb6abf2
Sohail <sohail65732@gmail.com>
2021-07-07 19:48:10 +0500

    Reset the logo

--- landing-website/src/Components/Logo.js
+++ landing-website/src/Components/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { logoImage } from '../Images/image'
 import logo from "../Images/logo1.png"
 
  function Logo() {
     return (
         <>
             <a href="#" class="navbar-logo" >
-                <img src={logo} />
+                <img src={logoImage} />
             </a>
         </>
     )
