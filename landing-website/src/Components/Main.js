a628e86769f20cc937e9e09991d38445e3b2536c
Sohail <sohail65732@gmail.com>
2021-07-07 18:40:25 +0500

    Setting main file structure while adding header tag

---
+++ landing-website/src/Components/Main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <>
+
+            <header>
+                <div class="header">
+
+                    <nav class="navigation">
+
+                    </nav>
+                </div>
+            </header>
+
+
+        </>
+    )
+}
+
+export default Main
