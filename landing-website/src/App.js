8c7056a7ebad145a8a9bc77fa2d8e3ded7659ebf
Sohail <sohail65732@gmail.com>
2021-07-07 18:53:02 +0500

    Rendring Main component in App.js and placing it in Router in order to make it SPA

--- landing-website/src/App.js
+++ landing-website/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './Components/Main';
-import {}
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
-
+      <Router>
     <Main/>
+    </Router>
     </div>
   );
 }
