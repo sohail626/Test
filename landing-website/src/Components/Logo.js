0abecef06c151f29f46135725fcb652ec26bbff5
Sohail <sohail65732@gmail.com>
2021-07-07 18:55:22 +0500

    Making a logo component and making an image folder in src directory

--- landing-website/src/Components/Logo.js
+++ landing-website/src/Components/Logo.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import logo from "../src/images/PinClipart.com_church-anniversary-clip-art_1458487.png"
 
 export default function Logo() {
     return (
         <>
-            
+            <a href="#" class="navbar-logo" >
+                <img src={logo} />
+            </a>
         </>
     )
 }
